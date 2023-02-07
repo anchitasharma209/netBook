26252dc0fbc6e46574e2d206dd7284d1d581348d
Anchita <anchitasharma86@gmail.com>
2023-02-07 20:09:27 +0530

    third

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express Controller is working</h1>')
+    return res.render('home',{
+        tittle:"Home"
+    })
 }
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes')
+app.set('view engine',"ejs")
+app.set('views','./views')
+
 app.use('/',router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
